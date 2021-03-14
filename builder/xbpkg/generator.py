f3cfa0efb8aea3166102e93aa9afb8a16960d5cd
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-03-14 11:35:54 +0530

    💝 Dynamic Cursor build

--- builder/xbpkg/generator.py
+++ builder/xbpkg/generator.py
@@ -2,25 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, NamedTuple
 
 from clickgen.builders import WindowsCursor, XCursor
 from clickgen.core import CursorAlias
 from clickgen.packagers import WindowsPackager, XPackager
-
-from xbpkg.constants import AUTHOR, COMMENT, THEME_NAME, URL
+from xbpkg.constants import AUTHOR, URL
 from xbpkg.symlinks import add_missing_xcursor
 
 
-def xbuild(
-    config: Dict[str, Dict[str, Any]],
-    x_out_dir: Path,
-) -> None:
+class Info(NamedTuple):
+    name: str
+    comment: str
+
+
+def xbuild(config: Dict[str, Dict[str, Any]], x_out_dir: Path, info: Info) -> None:
     """Build `XCursor-Pro` cursor theme for only `X11`(UNIX) platform.
 
     :config: (Dict) `XCursor-Pro` configuration.
 
     :x_out_dir: (Path) Path to the output directory, Where the `X11` cursor theme package will generate. It also creates a directory if not exists.
+
+    :info: (Dict) Content theme name & comment
     """
 
     for _, item in config.items():
@@ -35,15 +38,17 @@ def xbuild(
             XCursor.create(x_cfg, x_out_dir)
 
     add_missing_xcursor(x_out_dir / "cursors")
-    XPackager(x_out_dir, THEME_NAME, COMMENT)
+    XPackager(x_out_dir, info.name, info.comment)
 
 
-def wbuild(config: Dict[str, Dict[str, Any]], win_out_dir: Path) -> None:
+def wbuild(config: Dict[str, Dict[str, Any]], win_out_dir: Path, info: Info) -> None:
     """Build `XCursor-Pro` cursor theme for only `Windows` platforms.
 
     :config: (Dict) `XCursor-Pro` configuration.
 
     :win_out_dir: (Path) Path to the output directory, Where the `Windows` cursor theme package will generate. It also creates a directory if not exists.
+
+    :info: (Dict) Content theme name & comment
     """
 
     for _, item in config.items():
@@ -68,11 +73,11 @@ def wbuild(config: Dict[str, Dict[str, Any]], win_out_dir: Path) -> None:
                 print(f"Building '{win_cfg.stem}' Windows Cursor...")
                 WindowsCursor.create(win_cfg, win_out_dir)
 
-    WindowsPackager(win_out_dir, THEME_NAME, COMMENT, AUTHOR, URL)
+    WindowsPackager(win_out_dir, info.name, info.comment, AUTHOR, URL)
 
 
 def build(
-    config: Dict[str, Dict[str, Any]], x_out_dir: Path, win_out_dir: Path
+    config: Dict[str, Dict[str, Any]], x_out_dir: Path, win_out_dir: Path, info: Info
 ) -> None:
     """Build `XCursor-Pro` cursor theme for `X11` & `Windows` platforms.
 
@@ -81,6 +86,8 @@ def build(
     :x_out_dir: (Path) Path to the output directory, Where the `X11` cursor theme package will generate. It also creates a directory if not exists.
 
     :win_out_dir: (Path) Path to the output directory, Where the `Windows` cursor theme package will generate. It also creates a directory if not exists.
+
+    :info: (Dict) Content theme name & comment
     """
 
     def win_build(item: Dict[str, Any], alias: CursorAlias) -> None:
@@ -111,6 +118,6 @@ def build(
                 win_build(item, alias)
 
     add_missing_xcursor(x_out_dir / "cursors")
-    XPackager(x_out_dir, THEME_NAME, COMMENT)
+    XPackager(x_out_dir, info.name, info.comment)
 
-    WindowsPackager(win_out_dir, THEME_NAME, COMMENT, AUTHOR, URL)
+    WindowsPackager(win_out_dir, info.name, info.comment, AUTHOR, URL)
