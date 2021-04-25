edac68608afe14f7bf3011021ade6dd2a0ac3fe2
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-04-25 16:40:11 +0530

    ⚡ relative import module

--- builder/xcbuild/generator.py
+++ builder/xcbuild/generator.py
@@ -7,8 +7,8 @@ from typing import Any, Dict, NamedTuple
 from clickgen.builders import WindowsCursor, XCursor
 from clickgen.core import CursorAlias
 from clickgen.packagers import WindowsPackager, XPackager
-from xbpkg.constants import AUTHOR, URL
-from xbpkg.symlinks import add_missing_xcursor
+from .constants import AUTHOR, URL
+from .symlinks import add_missing_xcursor
 
 
 class Info(NamedTuple):
